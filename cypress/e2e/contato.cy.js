1b8e81cf7dd2a13c251074af0e4bf03da5ba083f
Thais Oliveira <thaisfernandes06@gmail.com>
2026-07-14 21:54:05 -0300

    Automação do Contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,62 @@
+describe('Funcionalidade: Contato', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/index.html')
+  });
+
+  it('Deve preencher formulário de contato com sucesso', () => {
+      cy.get('[name="name"]').type('Thais Oliveira')
+      cy.get('[name="email"]').type('thaisfernandes06@gmail.com')
+      cy.get('[name="subject"]').select('Sugestões')
+      cy.get('[name="message"]').type('Mensagem de teste')
+      cy.get('#btn-submit').click()
+      
+      //Resultado esperado
+      cy.contains('Contato enviado com sucesso!').should('exist')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher Nome', () => {
+      cy.get('[name="name"]').clear()
+      cy.get('[name="email"]').type('thaisfernandes06@gmail.com')
+      cy.get('[name="subject"]').select('Sugestões')
+      cy.get('[name="message"]').type('Mensagem de teste')
+      cy.get('#btn-submit').click()
+
+      //Resultado esperado
+      cy.get('#alert-container').should('contain', 'Por favor, preencha o campo Nome')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher email', () => {
+      cy.get('[name="name"]').type('Thais Oliveira')
+      cy.get('[name="email"]').clear()
+      cy.get('[name="subject"]').select('Sugestões')
+      cy.get('[name="message"]').type('Mensagem de teste')
+      cy.get('#btn-submit').click()
+
+      //Resultado esperado
+      cy.get('#alert-container').should('contain', 'Por favor, preencha o campo E-mail.')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem selecionar o assunto', () => {
+      cy.get('[name="name"]').type('Thais Oliveira')
+      cy.get('[name="email"]').type('thaisfernandes06@gmail.com')
+      //cy.get('[name="subject"]').select('Sugestões')
+      cy.get('[name="message"]').type('Mensagem de teste')
+      cy.get('#btn-submit').click()
+
+      //Resultado esperado
+      cy.get('#alert-container').should('contain', 'Por favor, selecione o Assunto.')
+  });
+
+   it('Deve validar mensagem de erro ao enviar sem preencher a mensagem', () => {
+      cy.get('[name="name"]').type('Thais Oliveira')
+      cy.get('[name="email"]').type('thaisfernandes06@gmail.com')
+      cy.get('[name="subject"]').select('Sugestões')
+      cy.get('[name="message"]').clear()
+      cy.get('#btn-submit').click()
+
+      //Resultado esperado
+      cy.get('#alert-container').should('contain', 'Por favor, escreva sua Mensagem.')
+  });
+
+});
